352dbbfeb10138087139352a6e40b6ae0242a4d0
albert04g28 <alberto.apariciogallo@alum.uca.es>
2023-11-14 16:23:12 +0100

    puto

--- codigo 3.c
+++ codigo 3.c
@@ -3,18 +3,43 @@
 
 int main(){
     
-    
+ /*int i;   
    
-for (int i = 1; i <= 10; i++) {
+for ( i = 1; i <= 10; i++) {
         
        
-printf("%d\n", i);  // Imprime el valor actual de i
+printf("%d\n", i); 
     }
 
     
    
 
    
+
+
+
+
+
+*/
+/*unsigned i;
+i=1;
+while (i<=10)
+{
+   printf("%u",i);
+   i++;
+}
+*/
+/*
+unsigned i;
+i=1;
+
+do
+{
+   printf("%u",i);
+   i++;
+} while (i<=10);
+*/
+
 
 
 
